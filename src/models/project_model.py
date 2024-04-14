45c473244f29ea79f0dfe25c312d5a2264f26649
bhamichi <b.hamichi@gmail.com>
2024-04-14 21:55:25 +0200

    add lead on project model

--- src/models/project_model.py
+++ src/models/project_model.py
@@ -1,12 +1,18 @@
 # Create model attributes/columns
 
-from sqlalchemy import Column, Integer, String
+from datetime import timezone
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 from ..database import Base
 
 class Project(Base):
     __tablename__ = "project"
 
     id = Column(Integer, primary_key=True)
+    icon_path = Column(String, index=True)
     name = Column(String(100), unique=True, index=True)
     key = Column(String(10), unique=True, index=True)
     description = Column(String, index=True)
+    lead_id = Column(Integer, ForeignKey('user.id'))  # ForeignKey to User model
+    lead = relationship("User", back_populates="projects")  # Assuming User model has 'projects' relationship
+    created_at = Column(DateTime, default=timezone.utc)
